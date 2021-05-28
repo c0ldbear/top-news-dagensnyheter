8c368aef125ac4eb7bb88f44ed3e2d9a0ea516df
teddy <teddyjh@protonmail.com>
2021-05-29 00:23:53 +0200

    change name of "main"-function: GrabNewsUrls -> GrabTopNewsUrls

--- FetchTopNewsDagensNyheter.py
+++ FetchTopNewsDagensNyheter.py
@@ -2,7 +2,7 @@ import aiohttp
 from bs4 import BeautifulSoup
 import asyncio
 
-async def GrabNewsUrls():
+async def GrabTopNewsUrls():
     '''
     Get the news article links in a form that can be clickable
     Input:      None
@@ -19,7 +19,7 @@ async def fetchNews():
     Output:     rText - HTML-text/-code as text
     Effect:     Get the HTML-text/-code from DN.se's website /nyhetsdygnet as text
     '''
-    url = "https://www.dn.se/"
+    url = "https://www.dn.se/" ## Get the front page of Dagens Nyheter to grab top popular news
     async with aiohttp.ClientSession() as session:
         async with session.get(url) as response:
             rText = await response.text()
@@ -34,8 +34,6 @@ async def findNewsTopics(responseText):
     Effect:     parses responseText to find the links and adds them to a list
     '''
     soup = BeautifulSoup(responseText, "html.parser")
-    #news_timeline = soup.find("div", attrs={'class':'timeline-page'})
-    #news_links = news_timeline.find_all("a", href=True)
     news_links = soup.find_all("a", href=True, class_="teaser")
     newsUrlsList = []
     for link in news_links:
@@ -59,11 +57,9 @@ async def main():
     newsUrlString = ""
     for news in News[1:6]:
         newsUrlString += "https://www.dn.se" + str(news) + "\n"
-    #newsUrlString += "\n" + "https://www.dn.se/nyhetsdygnet" + "\n"
     print(newsUrlString)
     print("\n Number of news: " + str(len(News)))
 
 if __name__ == "__main__":
     loop = asyncio.get_event_loop()
-    #loop.run_until_complete(main())
     loop.run_until_complete(testTopics())
